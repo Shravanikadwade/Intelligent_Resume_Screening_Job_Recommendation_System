5405af1e581b09dd6acc1ba7351dd5d5afa1a7f3
Shravanikadwade <shravanik704@gmail.com>
2026-08-05 23:54:58 +0530

    removed default vite assests

---
+++ ai_resume_system/src/test/java/com/aiResumeApplication/ai_resume_system/AiResumeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiResumeApplication.ai_resume_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
